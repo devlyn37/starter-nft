c88a178d3a82a4796c281051c848785daed2b410
Devlyn Dorfer <devlyndorfer@gmail.com>
2022-05-07 22:25:07 -0400

    initial setup + conversion to solmate erc721 contract

---
+++ src/Crossmint.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.13;
+
+import "solmate/src/tokens/ERC721.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+// Remember you've got some metadata here
+// "https://gateway.pinata.cloud/ipfs/QmPux5QgyPHfxjuCBf1GL6bnbYRoDdzwh9UGdnz2UXx58D";
+
+/// @notice Dummy contract for testing
+
+contract Crossmint is ERC721, Ownable {
+    uint256 public constant MAX_SUPPLY = 100000;
+    uint256 public salePrice = 0.001 ether;
+    uint256 public totalSupply;
+    string private uri;
+
+    address _crossmintAddress = 0xdAb1a1854214684acE522439684a145E62505233;
+
+    // MODIFIERS
+
+    modifier isCorrectPayment() {
+        require(salePrice == msg.value, "Incorrect ETH value sent");
+        _;
+    }
+
+    modifier isAvailable() {
+        require(totalSupply + 1 <= MAX_SUPPLY, "No more left");
+        _;
+    }
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        string memory _uri
+    ) payable ERC721(name, symbol) {
+        uri = _uri;
+    }
+
+    // PUBLIC
+
+    function mint() external payable isCorrectPayment isAvailable {
+        _safeMint(msg.sender, totalSupply++);
+    }
+
+    function crossmint(address to)
+        external
+        payable
+        isCorrectPayment
+        isAvailable
+    {
+        require(
+            msg.sender == _crossmintAddress,
+            "This function is for Crossmint only."
+        );
+        _safeMint(to, totalSupply++);
+    }
+
+    // ADMIN
+
+    function setBaseURI(string calldata baseURI) external onlyOwner {
+        uri = baseURI;
+    }
+
+    function setSalePrice(uint256 price) external onlyOwner {
+        salePrice = price;
+    }
+
+    // VIEW
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        return uri;
+    }
+}
